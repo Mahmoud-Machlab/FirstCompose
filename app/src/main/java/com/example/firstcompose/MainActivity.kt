70f8bdfb0df22c5c187384f8e0c1a5180baf4af9
Frank <f.neumann.berlin@googlemail.com>
2023-04-25 12:09:51 +0200

    Fertig

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.firstcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.textclassifier.ConversationAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -17,14 +27,19 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.firstcompose.ui.theme.FirstComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,27 +51,87 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Neumann")
-
+//                    MessageCard(Message("Frank","ein kleiner Text"))
+                    Conversation(messages = messageList)
                 }
             }
         }
     }
 }
 
+data class Message(val author:String, val body:String)
+
+val messageList = listOf(
+    Message("Hans","ein kleiner Text um das Beispiel besser zu erklären"),
+    Message("Hans","ein kleiner Text um das Beispiel besser zu erklären"),
+    Message("Hans","ein kleiner Text um das Beispiel besser zu erklären"),
+    Message("Hans","ein kleiner Text um das Beispiel besser zu erklären ein kleiner Text um das Beispiel besser zu erklären ein kleiner Text um das Beispiel besser zu erklären"),
+    Message("Hans","ein kleiner Text um das Beispiel besser zu erklären"),
+    Message("Hans","ein kleiner Text um das Beispiel besser zu erklären ein kleiner Text um das Beispiel besser zu erklären ein kleiner Text um das Beispiel besser zu erklären"),
+    Message("Hans","ein kleiner Text um das Beispiel besser zu erklären"),
+    Message("Hans","ein kleiner Text um das Beispiel besser zu erklären"),
+    Message("Hans","ein kleiner Text um das Beispiel besser zu erklären"),
+    Message("Hans","ein kleiner Text um das Beispiel besser zu erklären"),
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(msg:Message, modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier.padding(all =  16.dp),
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.einstein),
+
+            contentDescription = "Profilbild von Einstein",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpandet by remember { mutableStateOf(false) }
+        Column(
+            modifier = Modifier.clickable { isExpandet = !isExpandet }
+        ) {
+            Text(
+                text = msg.author,
+                modifier = modifier,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = msg.body,
+                maxLines = if (isExpandet) Int.MAX_VALUE else 2,
+                style = MaterialTheme.typography.bodyMedium,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Conversation(messages: List<Message>){
+    LazyColumn{
+        items(messages){message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview(name = "Tagmodus",
+    showBackground = true)
+//@Preview(name = "Nachtmodus",
+//    showBackground = true,
+//    uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun GreetingPreview() {
     FirstComposeTheme {
-        Greeting("Frank")
+        Conversation(messages = messageList)
+//        MessageCard(Message("Frank","ein kleiner Text"))
 //        WeatherScreen()
     }
 }
